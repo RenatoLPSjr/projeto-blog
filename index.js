4b758af5cf69d89d8492e5be6dcfdca54a627a23
RenatoLPSjr <renato.lop.jr@gmail.com>
2023-01-07 00:23:30 -0300

    Adicionando arquivos e pastas essenciais

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//View engine
+app.set('view engine', 'ejs');
+
+//Static
+app.use(express.static('public'));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("O servidor está rodando!");
+});
