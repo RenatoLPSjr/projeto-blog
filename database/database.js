6224745eb72a60bc69306b1ccec40303e3b2c8bf
RenatoLPSjr <renato.lop.jr@gmail.com>
2023-01-08 12:51:47 -0300

    Criando o artigo

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('guiapress','root','38242628',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-3:00"
 });
 
 module.exports = connection;
