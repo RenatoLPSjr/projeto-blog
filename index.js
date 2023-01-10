c0eea5a49248f58d5b62662b62f85720f482e248
RenatoLPSjr <renato.lop.jr@gmail.com>
2023-01-09 21:38:50 -0300

    Criando home page e leitura do blog

--- index.js
+++ index.js
@@ -33,8 +33,36 @@ app.use("/",categoriesController);
 app.use("/",articleController);
 
 app.get("/", (req, res)=>{
-    res.render("index");
+    Article.findAll({
+        order:[
+            ['id', 'DESC']
+        ]
+    }).then(articles =>{
+
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
+    });
 });
+
+app.get("/:slug",(req, res) =>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err =>{
+        res.redirect("/");
+    });
+})
 
 app.listen(8080, () => {
     console.log("O servidor está rodando!");
