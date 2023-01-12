b726762adf5c91bde1d651ded3fdd9f9edef3b23
RenatoLPSjr <renato.lop.jr@gmail.com>
2023-01-12 19:59:00 -0300

    Arquivos essesncias do sistema de login

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articleController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController");
 
 const Category = require("./categories/Category");
 const Article = require("./articles/Article");
+const User = require("./users/User");
 
 //View engine
 app.set('view engine', 'ejs');
@@ -31,6 +33,7 @@ connection
 
 app.use("/",categoriesController);
 app.use("/",articleController);
+app.use("/", usersController);
 
 app.get("/", (req, res)=>{
     Article.findAll({
