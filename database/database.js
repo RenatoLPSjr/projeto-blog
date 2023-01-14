264a76f6d9cc8491bbc0556e5909cd7389b4833e
Renato Lopes Junior <89666395+RenatoLPSjr@users.noreply.github.com>
2023-01-14 10:29:21 -0300

    Delete database.js

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('guiapress','root','38242628',{
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: "-3:00"
-});
-
-module.exports = connection;
